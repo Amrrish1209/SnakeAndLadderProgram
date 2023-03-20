be7245ce2049850944831540b000258e79920579
Amrrish1209 <roshanbabubl@gmail.com>
2023-03-20 19:47:03 +0530

    Add: Snake and Ladder game played with single player at starting postion 0

---
+++ src/com/bridgelabz/snakeandladderstimulator/SnakeAndLadder.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.snakeandladderstimulator;
+
+public class SnakeAndLadder {
+
+	public static void main(String[] args) {
+		
+		int position = 0;
+		System.out.println("Starting Position "+position);
+		
+	}
+}
