2c782fd353a336582b52957809b4daa89699a6ac
Amrrish1209 <roshanbabubl@gmail.com>
2023-03-21 08:26:20 +0530

    Add: The Player rolls a dice to get the number between 1 to 6 using random

--- src/com/bridgelabz/snakeandladderstimulator/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeandladderstimulator/SnakeAndLadder.java
@@ -1,11 +1,18 @@
 package com.bridgelabz.snakeandladderstimulator;
 
+import java.util.Random;
+
 public class SnakeAndLadder {
 
 	public static void main(String[] args) {
+
+		int startposition = 0;
+		System.out.println("Starting Position " + startposition);
 		
-		int position = 0;
-		System.out.println("Starting Position "+position);
+		Random random = new Random();
 		
+		// Generate a random number between 1 and 6
+		int dieRoll = random.nextInt(6) + 1;
+		System.out.println("The die rolled and landed on: " + dieRoll);
 	}
 }
