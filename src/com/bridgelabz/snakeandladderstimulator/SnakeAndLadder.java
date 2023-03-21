4ae7a757a25a811f538c6ea768cda4ef965a8515
Amrrish1209 <roshanbabubl@gmail.com>
2023-03-21 09:45:33 +0530

    Add: Report the number of times the dice was played to win the game and also the position after every die role in java

--- src/com/bridgelabz/snakeandladderstimulator/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeandladderstimulator/SnakeAndLadder.java
@@ -5,11 +5,12 @@ public class SnakeAndLadder {
     public static void main(String[] args) {
         int winningposition = 100;
         int currentposition = 0;
+        int rollcount=0;
 
         while (currentposition < winningposition) {
             // Roll the dice to get a number between 1 to 6
             int diceRoll = (int) Math.floor(Math.random() * 6) + 1;
-
+            rollcount++;
             // Check for option using random number between 0 to 2
             int option = (int) Math.floor(Math.random() * 3);
 
@@ -25,15 +26,18 @@ public class SnakeAndLadder {
                 case 2:
                 	//Player behind by the number of positions received on the die
                     newposition = newposition - diceRoll;
+                    break;
             }
          // Ensure that the player does not move beyond the winning position
             if (newposition > winningposition) {
                 newposition = currentposition;
-                continue;
+            }
+            else if(newposition<0) {
+            	newposition=0;
             }
             // Update the current position
             currentposition=newposition;
-            System.out.println("The Player is now at position "+currentposition);
+            System.out.println("After roll: "+rollcount+", Player is now at position: "+currentposition);
         }
 
         System.out.println("The player has won the game!");
