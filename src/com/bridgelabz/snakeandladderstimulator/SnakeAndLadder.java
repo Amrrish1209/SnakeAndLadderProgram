7612452aee9ec08716edcaf32c193607d0b179ef
Amrrish1209 <roshanbabubl@gmail.com>
2023-03-21 09:27:46 +0530

    Add: Reach till the player reaches the winning position 100

--- src/com/bridgelabz/snakeandladderstimulator/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeandladderstimulator/SnakeAndLadder.java
@@ -1,46 +1,39 @@
 package com.bridgelabz.snakeandladderstimulator;
 
-import java.util.Random;
-
 public class SnakeAndLadder {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
+        int winningposition = 100;
+        int currentposition = 0;
 
-		Random random = new Random();
-		// Generate a random number between 1 and 6
-		int dieRoll = random.nextInt(6) + 1;
-		// Generate a random number between 0 to 2
-		int option = random.nextInt(3);
-		int currentposition = 0;
-		int newposition = currentposition;
-		switch (option) {
-		case 0:
-			// No play the player stays in the same position
-			break;
-		case 1:
-			// player moves ahead by the number of position received in the die
-			newposition = currentposition + dieRoll;
-			break;
-		case 2:
-			// player moves behind by the number of position received in the die
-			newposition = currentposition - dieRoll;
+        while (currentposition < winningposition) {
+            // Roll the dice to get a number between 1 to 6
+            int diceRoll = (int) Math.floor(Math.random() * 6) + 1;
 
-		}
-		if (newposition < 0) {
-			newposition = currentposition;
-		}
-		// print the result
-		System.out.println("The die rolled and landed on: " + dieRoll);
-		switch (option) {
-		case 0:
-			System.out.println("The player stays in the same position: " + currentposition);
-			break;
-		case 1:
-			System.out.println("The player climbed a ladder and moved to position: " + newposition);
-			break;
-		case 2:
-			System.out.println("The player encountered a snake and moved back to position: " + newposition);
-			break;
-		}
-	}
+            // Check for option using random number between 0 to 2
+            int option = (int) Math.floor(Math.random() * 3);
+
+            int newposition=currentposition;
+            switch (option) {
+                case 0:
+                	// No play,player stays in the same position 
+                    break;
+                case 1:
+                	//player ahead by the number of positions received on the die
+                    newposition = newposition + diceRoll;
+                    break;
+                case 2:
+                	//Player behind by the number of positions received on the die
+                    newposition = newposition - diceRoll;
+            }
+
+            // Update the current position
+            currentposition=newposition;
+            System.out.println("The Player is now at position "+currentposition);
+        }
+
+        System.out.println("The player has won the game!");
+    }
 }
+
+
