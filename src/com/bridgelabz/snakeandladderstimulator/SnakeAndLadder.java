4a0d6175395dffaac24030df83637f617b6fb4da
Amrrish1209 <roshanbabubl@gmail.com>
2023-03-21 08:53:22 +0530

    Add: The player then check for an option.

--- src/com/bridgelabz/snakeandladderstimulator/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeandladderstimulator/SnakeAndLadder.java
@@ -6,13 +6,41 @@ public class SnakeAndLadder {
 
 	public static void main(String[] args) {
 
-		int startposition = 0;
-		System.out.println("Starting Position " + startposition);
-		
 		Random random = new Random();
-		
 		// Generate a random number between 1 and 6
 		int dieRoll = random.nextInt(6) + 1;
+		// Generate a random number between 0 to 2
+		int option = random.nextInt(3);
+		int currentposition = 0;
+		int newposition = currentposition;
+		switch (option) {
+		case 0:
+			// No play the player stays in the same position
+			break;
+		case 1:
+			// player moves ahead by the number of position received in the die
+			newposition = currentposition + dieRoll;
+			break;
+		case 2:
+			// player moves behind by the number of position received in the die
+			newposition = currentposition - dieRoll;
+
+		}
+		if (newposition < 0) {
+			newposition = currentposition;
+		}
+		// print the result
 		System.out.println("The die rolled and landed on: " + dieRoll);
+		switch (option) {
+		case 0:
+			System.out.println("The player stays in the same position: " + currentposition);
+			break;
+		case 1:
+			System.out.println("The player climbed a ladder and moved to position: " + newposition);
+			break;
+		case 2:
+			System.out.println("The player encountered a snake and moved back to position: " + newposition);
+			break;
+		}
 	}
 }
